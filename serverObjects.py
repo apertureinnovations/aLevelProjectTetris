07ed871677fcda26b3768d004694aebfc5b43ce1
Jake Palmer <jake.palmer@apertureinnovations.co.uk>
2022-09-16 18:01:49 +0100

    The serverObjects file should be working - need to create a server to test first.

--- serverObjects.py
+++ serverObjects.py
@@ -1,11 +1,24 @@
+import socket
+import json
+
 class SGame:
     def __init__(self):
-
+        self.hostName = 0.0.0.0
+        self.port = 56365
+        self.handler = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.handler.connect((self.hostName, self.port))
         pass
 
     def request(self, data):
-
-        pass
+        self.handler.sendall(data.encode)
+        running = True
+        while running:
+            Data = self.handler.recv(1024)
+            if not Data:
+                continue
+            else:
+                running = False
+                return json.loads(Data.decode())
 
     def getCurrentTetrominoState(self):
         return self.request("currentTetrominoState")
